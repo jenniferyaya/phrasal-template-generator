3541e7e1eed740157c8c810d06dbd0e1a129a628
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-02-27 13:14:06 -0500

    Cleaned up files as per Markbot

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var colour = 'A colour (orange, indigo, etc.)';
 
 var options = prompt(colour);
-
 document.write(options);
 
+
+var text = 'After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.';
+var adjective = '';
